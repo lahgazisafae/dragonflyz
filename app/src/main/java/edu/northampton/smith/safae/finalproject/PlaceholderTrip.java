3dd027ec7c381a13083adf78018d4a635642df34
lahgazisafae <lahga22s@mtholyoke.edu>
2017-02-28 22:49:02 -0500

    first commit

---
+++ app/src/main/java/edu/northampton/smith/safae/finalproject/PlaceholderTrip.java
@@ -0,0 +1,27 @@
+package edu.northampton.smith.safae.finalproject;
+
+/**
+ * Created by Safae on 2/28/2017.
+ */
+
+public class PlaceholderTrip {
+
+
+    String destination;
+
+    public PlaceholderTrip(String destination){
+        this.destination = destination;
+    }
+
+
+    public String getDestination() {
+        return destination;
+    }
+
+
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+}

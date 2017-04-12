be5f6d79f2c4e0898e93bd25054f82ea1c85f4bc
Amyrah Ruby <aarroyo@smith.edu>
2017-04-12 11:29:53 -0400

    fixed the list activity
    changed the object type of the time information to a string for easier
    manipulation

--- app/src/main/java/edu/northampton/smith/safae/finalproject/Trips_SQLite.java
+++ app/src/main/java/edu/northampton/smith/safae/finalproject/Trips_SQLite.java
@@ -19,6 +19,7 @@ public class Trips_SQLite extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip__sqlite);
         //this.deleteDatabase("users");
+
         ds = new DataSource(this);
         ds.open();
         List<Trip> values = ds.getAllTrips();
@@ -38,13 +39,13 @@ public class Trips_SQLite extends ListActivity {
         }
         if(view.getId() == R.id.add){
 
-            String [] destinations = {"Budapest","Prague","Madrid","Shanghai","Athens"};
-            String [] dates_arrival = {"2261995","1231993","4111993","4271972","12312017"};
-            String [] dates_departure = {"2261995","1231993","4111993","4271972","12312017"};
-            String [] entry_time = {"1234","2341","1156","1034","1545"};
+            String [] destination = {"Budapest","Prague","Madrid","Shanghai","Athens"};
+            String [] date_departure = {"2261995","1231993","4111993","4271972","12312017"};
+            String [] date_return = {"2261995","1231993","4111993","4271972","12312017"};
+            String [] diary_entry_time = {"12:34","23:41","11:56","10:34","15:45"};
             int index = new Random().nextInt(4);
-            t = ds.createTrip(trips[index],trip_departure[index],
-                    trip_return[index],entry_time[index]);
+            t = ds.createTrip(destination[index],date_departure[index],
+                    date_return[index],diary_entry_time[index]);
 
             adapter.add(t);
 

158d2f6dc5a387c2aa629413a34bafce69529db7
Amyrah Ruby <aarroyo@smith.edu>
2017-04-26 09:34:09 -0400

    validations
    set up edit text validation and both datePicker validations

--- app/src/main/java/edu/northampton/smith/safae/finalproject/Depart.java
+++ app/src/main/java/edu/northampton/smith/safae/finalproject/Depart.java
@@ -29,14 +29,15 @@ import java.util.Date;
 public class Depart extends Fragment implements View.OnClickListener {
 
 
-    Trip currentTrip = null;
+
 
     private Calendar c;
-    private Button submit;
     private String date;
     private EditText place;
     DatePicker datePicker;
     private Button submit;
+
+    Date currDate;
  
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,22 +54,25 @@ public class Depart extends Fragment implements View.OnClickListener {
         c.get(Calendar.DATE);
         datePicker.setMinDate(c.get(Calendar.DATE)+ c.get(Calendar.MONTH)+c.get(Calendar.YEAR));
 
+        currDate = new Date();
+
         submit = (Button) v.findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 final String location = place.getText().toString();
-
-                if (location.matches("")){
-                    place.setError("Invalid Location!");
-                }
-
-                Date d = new Date(datePicker.getYear(), datePicker.getMonth() , datePicker.getDayOfMonth());
+                Date d = new Date(datePicker.getYear()-1900, datePicker.getMonth() , datePicker.getDayOfMonth());
                 SimpleDateFormat dform = new SimpleDateFormat("MM-dd-yyyy");
                 date = dform.format(d);
 
-                Fragment returnFragment = new Return();
+                if (location.matches("")){
+                    place.setError("Invalid Location!");
+                } else if (d.getDate()<currDate.getDate()){
+                    Toast.makeText(getActivity(),"Invalid date!",Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    Fragment returnFragment = new Return();
                     Bundle bundle = new Bundle();
                     bundle.putString("date_departure", date);
                     bundle.putString("location", location);
@@ -79,24 +83,12 @@ public class Depart extends Fragment implements View.OnClickListener {
                     fragmentTransaction.replace(R.id.fragment_container, returnFragment);
                     fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
+                }
             }
         });
 
         return v;
     }
-    public boolean isValidLocation(String location){
-
-        if(location!= null && !location.matches("")) {
-            System.out.println("and now location" + location);
-            return true;
-        }
-        else
-            return false;
-    }
-    public boolean isValidDate(){
-        return true;
-    }
-
     public void onStart(){
         super.onStart();
     }
